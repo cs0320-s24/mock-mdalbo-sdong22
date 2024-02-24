e99448d0bbe36de35ecc41b49c708232773884f5
simd22 <113197504+simd22@users.noreply.github.com>
2024-02-24 11:21:05 -0500

    refactor: added mock files

---
+++ mock/src/components/REPLHistory.tsx
@@ -0,0 +1,22 @@
+import "../styles/main.css";
+
+interface REPLHistoryProps {
+  // TODO: Fill with some shared state tracking all the pushed commands
+  history: string[];
+}
+export function REPLHistory(props: REPLHistoryProps) {
+  return (
+    <div className="repl-history">
+      {props.history.map(
+        (
+          elem,
+          i // uniquely identify each element of array
+        ) => (
+          <p key={i}>{elem}</p>
+        )
+      )}
+      {/* This is where command history will go */}
+      {/* TODO: To go through all the pushed commands... try the .map() function! */}
+    </div>
+  );
+}
