1b31f51c64acb6fefe21a19f63eb7cd2c551b89d
manolodalbo <manuel_dal_bo@brown.edu>
2024-02-24 12:27:48 -0500

    feat: adding user story 1 results

--- mock/src/components/REPL.tsx
+++ mock/src/components/REPL.tsx
@@ -15,16 +15,22 @@ import { REPLInput } from "./REPLInput";
 export default function REPL() {
   // TODO: Add some kind of shared state that holds all the commands submitted.
 
-  const [history, setHistory] = useState<string[]>([]);
+  const [history, setHistory] = useState<[string, string][]>([]);
+  const [isVerbose, setVerbose] = useState<boolean>(false);
 
   return (
     <div className="repl">
       {/*This is where your REPLHistory might go... You also may choose to add it within your REPLInput 
       component or somewhere else depending on your component organization. What are the pros and cons of each? */}
       {/* TODO: Update your REPLHistory and REPLInput to take in new shared state as props */}
-      <REPLHistory history={history} />
+      <REPLHistory isVerbose={isVerbose} history={history} />
       <hr></hr>
-      <REPLInput history={history} setHistory={setHistory} />
+      <REPLInput
+        history={history}
+        setHistory={setHistory}
+        isVerbose={isVerbose}
+        setVerbose={setVerbose}
+      />
     </div>
   );
 }
